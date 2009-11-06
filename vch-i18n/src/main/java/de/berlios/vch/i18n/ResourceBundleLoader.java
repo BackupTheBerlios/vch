2e931ec8b274bf0a844bfaf7d87bd6de26e9b681
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2009-11-06 14:45:15 +0000

    Initial import.
    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/branches/osgi@236 e742bc6a-2767-0410-a9b8-df252b6c4596

---
+++ vch-i18n/src/main/java/de/berlios/vch/i18n/ResourceBundleLoader.java
@@ -0,0 +1,25 @@
+package de.berlios.vch.i18n;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourceBundleLoader {
+
+    private static transient Logger logger = LoggerFactory.getLogger(ResourceBundleLoader.class);
+
+    public static ResourceBundle load(BundleContext ctx, Locale locale) throws IOException {
+        logger.debug("Loading messages for locale {} for bundle {}", locale.toString(), ctx.getBundle()
+                .getSymbolicName());
+        URL propsUrl = ctx.getBundle().getResource("/lang/lang_" + locale.toString() + ".properties");
+        InputStream in = propsUrl.openStream();
+        return new PropertyResourceBundle(in);
+    }
+}
