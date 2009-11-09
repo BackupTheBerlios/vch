be67b24dba2fa4acadfbc738395b616f0bcd0fc7
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2009-11-09 18:41:56 +0000

    use more information from the amf response
    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/branches/osgi@281 e742bc6a-2767-0410-a9b8-df252b6c4596

--- vch-parser-dmax/src/main/java/de/berlios/vch/parser/dmax/VideoPageParser.java
+++ vch-parser-dmax/src/main/java/de/berlios/vch/parser/dmax/VideoPageParser.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.htmlparser.Tag;
+import org.htmlparser.tags.ParagraphTag;
 import org.htmlparser.util.NodeIterator;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
@@ -29,17 +30,50 @@ public class VideoPageParser {
     private static transient Logger logger = LoggerFactory.getLogger(VideoPageParser.class);
     
     public IVideoPage parse(VideoPage page) throws Exception {
-        // TODO parse desc and thumb
         String pageContent = HttpUtils.get(page.getUri().toString(), null, DmaxParser.CHARSET);
-        Calendar pubCal = Calendar.getInstance();
-        pubCal.setTime(parsePubDate(pageContent));
+
+        // parse the video uri
         Map<String, Object> video = getVideo(pageContent);
-        page.setPublishDate(pubCal);
         page.setVideoUri(new URI((String) video.get("video")));
+        
+        // parse the duration
         page.setDuration( (Long)video.get("duration") );
+        
+        // use the publish date from amf or fallback to parse the html page
+        Calendar pubCal = Calendar.getInstance();
+        if(video.get("pubDate") != null) {
+            pubCal.setTime((Date) video.get("pubDate"));
+            page.setPublishDate(pubCal);
+        } else {
+            try {
+                pubCal.setTime(parsePubDate(pageContent));
+                page.setPublishDate(pubCal);
+            } catch (Exception e) {
+                logger.warn("Couldn't parse publish date: {}", e.getLocalizedMessage());
+                page.setPublishDate(Calendar.getInstance());
+            }
+        }
+        
+        // use the description from amf or fallback to parse the html page
+        if(video.get("description") != null) {
+            page.setDescription((String) video.get("description"));
+        } else {
+            page.setDescription(parseDescription(pageContent));
+        }
+        
+        // parse the thumb
+        if(video.get("thumb") != null) {
+            page.setThumbUri(new URI((String) video.get("thumb")));
+        }
+        
         return page;
     }
     
+    private String parseDescription(String pageContent) throws ParserException {
+        ParagraphTag p = (ParagraphTag) HtmlParserUtils.getTag(pageContent, DmaxParser.CHARSET, "div.vp-perpage-clipinfo p");
+        return p != null ? p.getStringText() : "";
+    }
+
     public Date parsePubDate(String pageContent) throws ParserException, ParseException {
         Tag dateTag = HtmlParserUtils.getTag(pageContent, DmaxParser.CHARSET, "div.vp-perpage-clipinfo ul li");
         String dateString = dateTag.toHtml();
@@ -58,21 +92,6 @@ public class VideoPageParser {
         
         return pubDate;
     }
-    
-//    public NodeList getChapterItemCells() throws ParserException {
-//        Div div = (Div) HtmlParserUtils.getTag(pageContent, DmaxParser.CHARSET, "div#vp-perpage-related-long-form-head");
-//        Div chapterContainer = (Div) div.getNextSibling().getNextSibling();
-//        return HtmlParserUtils.getTags(chapterContainer.toHtml(), DmaxParser.CHARSET, "div[class~=vp-promo-item]");
-//    }
-//    
-//    public String getFeedLink() throws Exception {
-//        LinkTag a = (LinkTag) HtmlParserUtils.getTag(pageContent, DmaxParser.CHARSET, "div#vp-perpage-videoinfo div[class~=vp-perpage-clipinfo] p a");
-//        if(a != null) {
-//            return a.getLink();
-//        } else {
-//            throw new Exception("Feed link not found");
-//        }
-//    }
     
     @SuppressWarnings("unchecked")
     public Map<String, Object> getVideo(String pageContent) throws Exception {
@@ -112,6 +131,9 @@ public class VideoPageParser {
         double length = (Double) mediaDTO.get("length");
         video.put("video", videoUri);
         video.put("duration", TimeUnit.MILLISECONDS.toSeconds((long) length));
+        video.put("description", mediaDTO.get("shortDescription"));
+        video.put("thumb", mediaDTO.get("thumbnailURL"));
+        video.put("pubDate", mediaDTO.get("publishedDate"));
         logger.debug("Video: {}, duration: {}", videoUri, length);
         return video;
     }
