a008f881730dd51f459997acc4c7157077f533ed
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2009-11-22 21:32:35 +0000

    Initial import.
    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/branches/osgi@295 e742bc6a-2767-0410-a9b8-df252b6c4596

---
+++ vch-download-http/src/main/java/de/berlios/vch/download/http/HttpDownloadFactory.java
@@ -0,0 +1,50 @@
+package de.berlios.vch.download.http;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
+import org.osgi.service.log.LogService;
+
+import de.berlios.vch.download.Download;
+import de.berlios.vch.download.DownloadFactory;
+import de.berlios.vch.parser.IVideoPage;
+
+@Component
+@Provides
+public class HttpDownloadFactory implements DownloadFactory {
+    
+    @Requires
+    private LogService logger;
+    
+    private boolean valid = false;
+    
+    public HttpDownloadFactory(LogService logger) {
+        this.logger = logger;
+    }
+    
+    @Override
+    public boolean accept(IVideoPage video) {
+        if(valid && video.getVideoUri() != null) {
+            return "http".equals(video.getVideoUri().getScheme());
+        }
+        return false;
+    }
+
+    @Override
+    public Download createDownload(IVideoPage page) {
+        return new HttpDownload(page, logger);
+    }
+    
+    @Validate
+    public void start() {
+        valid = true;
+        logger.log(LogService.LOG_INFO, "HttpDownloadFactory up and running");
+    }
+    
+    @Invalidate
+    public void stop() {
+        valid = false;
+    }
+}
