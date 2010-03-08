66c766a5760a24445ca61a93664da432bd3a68c4
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2010-03-08 19:57:22 +0000

    removed remote debugging params
    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/branches/osgi@456 e742bc6a-2767-0410-a9b8-df252b6c4596

--- vch/src/main/resources/vch.sh
+++ vch/src/main/resources/vch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd `dirname $0`
-#java -Djava.util.logging.config.file=conf/logging.properties -jar bin/org.apache.felix.main-${felix.main.version}.jar
-java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Djava.util.logging.config.file=conf/logging.properties -jar bin/org.apache.felix.main-${felix.main.version}.jar
+java -Djava.util.logging.config.file=conf/logging.properties -jar bin/org.apache.felix.main-${felix.main.version}.jar
+#java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Djava.util.logging.config.file=conf/logging.properties -jar bin/org.apache.felix.main-${felix.main.version}.jar
