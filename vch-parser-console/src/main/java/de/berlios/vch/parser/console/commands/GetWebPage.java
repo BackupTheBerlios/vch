8d2ed30cb836b5718f7725034b3274c2be2f1316
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2010-03-28 16:42:52 +0000

    * use the new IParserService
    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/branches/osgi@520 e742bc6a-2767-0410-a9b8-df252b6c4596

--- vch-parser-console/src/main/java/de/berlios/vch/parser/console/commands/GetWebPage.java
+++ vch-parser-console/src/main/java/de/berlios/vch/parser/console/commands/GetWebPage.java
@@ -1,22 +1,20 @@
 package de.berlios.vch.parser.console.commands;
 
 import java.io.PrintStream;
-import java.net.URI;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
 import java.util.Stack;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.felix.shell.Command;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceException;
 import org.osgi.util.tracker.ServiceTracker;
 
 import de.berlios.vch.parser.IOverviewPage;
+import de.berlios.vch.parser.IParserService;
 import de.berlios.vch.parser.IVideoPage;
 import de.berlios.vch.parser.IWebPage;
 import de.berlios.vch.parser.IWebParser;
-import de.berlios.vch.parser.OverviewPage;
-import de.berlios.vch.parser.WebPageTitleComparator;
 
 public class GetWebPage implements Command {
 
@@ -28,17 +26,22 @@ public class GetWebPage implements Command {
 
     public GetWebPage(BundleContext context) {
         ctx = context;
-        st = new ServiceTracker(ctx, IWebParser.class.getName(), null);
+        st = new ServiceTracker(ctx, IParserService.class.getName(), null);
         st.open();
     }
 
     @Override
     public void execute(String cmd, PrintStream out, PrintStream err) {
         try {
+            IParserService parserService = (IParserService) st.getService();
+            if(parserService == null) {
+                throw new ServiceException("ParserService not available");
+            }
+            
             String[] args = cmd.split(" ");
             if (args.length == 1) {
                 if (menuStack.isEmpty()) {
-                    IOverviewPage page = getParsers();
+                    IOverviewPage page = parserService.getParserOverview();
                     menuStack.push(page);
                 }
                 showMenu(out, err, menuStack.peek());
@@ -64,16 +67,11 @@ public class GetWebPage implements Command {
                         for (IWebPage page : opage.getPages()) {
                             int pageId = (Integer) page.getUserData().get("menu_id");
                             if (pageId == id) {
-                                IWebParser parser = getParser(page.getParser());
+                                IWebParser parser = parserService.getParser(page.getParser());
                                 if(parser != null) {
                                     out.println("Loading page. Please wait...");
-                                    if (page.getUri() != null && "vchpage://localhost".equals(page.getUri().toString())) {
-                                        menuStack.push(parser.getRoot());
-                                        showMenu(out, err, menuStack.peek());
-                                    } else {
-                                        menuStack.push(parser.parse(page));
-                                        showMenu(out, err, menuStack.peek());
-                                    }
+                                    menuStack.push(parserService.parse(page.getVchUri()));
+                                    showMenu(out, err, menuStack.peek());
                                     return;
                                 } else {
                                     err.println("This parser is not available anymore");
@@ -83,7 +81,7 @@ public class GetWebPage implements Command {
 
                         err.println("Page with ID " + id + " not found");
                     } else {
-                        IWebParser parser = getParser(currentPage.getParser());
+                        IWebParser parser = parserService.getParser(currentPage.getParser());
                         if(parser != null) {
                             IWebPage nextLevel = parser.parse(currentPage);
                             showMenu(out, err, nextLevel);
@@ -147,35 +145,5 @@ public class GetWebPage implements Command {
     @Override
     public String getUsage() {
         return getName() + " [<id> | ..]";
-    }
-
-    private IOverviewPage getParsers() throws Exception {
-        Object[] parsers = st.getServices();
-        IOverviewPage overview = new OverviewPage();
-        if (parsers != null && parsers.length > 0) {
-            for (Object o : parsers) {
-                IWebParser parser = (IWebParser) o;
-                IOverviewPage parserPage = new OverviewPage();
-                parserPage.setTitle(parser.getTitle());
-                parserPage.setUri(new URI("vchpage://localhost"));
-                parserPage.setParser(parser.getId());
-                overview.getPages().add(parserPage);
-            }
-        }
-        
-        Collections.sort(overview.getPages(), new WebPageTitleComparator());
-        return overview;
-    }
-    
-    public IWebParser getParser(String id) {
-        Object[] parsers = st.getServices();
-        for (Object o : parsers) {
-            IWebParser parser = (IWebParser) o;
-            if(id.equals(parser.getId())) {
-                return parser;
-            }
-        }
-        
-        return null;
     }
 }
