9d1e822aecff427421cc788588f38f42dda4ba22
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2010-04-02 11:16:51 +0000

    * added preferIPv4Stack property
    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/branches/osgi@525 e742bc6a-2767-0410-a9b8-df252b6c4596

--- vch/src/main/resources/vch.sh
+++ vch/src/main/resources/vch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd `dirname $0`
-java -Djava.util.logging.config.file=conf/logging.properties -jar bin/org.apache.felix.main-${felix.main.version}.jar
-#java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Djava.util.logging.config.file=conf/logging.properties -jar bin/org.apache.felix.main-${felix.main.version}.jar
+
+java -Djava.net.preferIPv4Stack=true \
+     -Djava.util.logging.config.file=conf/logging.properties \
+     -jar bin/org.apache.felix.main-${felix.main.version}.jar
+
+#java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 \
+#     -Djava.net.preferIPv4Stack=true \
+#     -Djava.util.logging.config.file=conf/logging.properties \
+#     -jar bin/org.apache.felix.main-${felix.main.version}.jar
