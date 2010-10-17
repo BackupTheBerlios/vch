66873d2ebbdc062d46ff5138573fae7bcc233214
Henrik Niehaus <henrik.niehaus@gmx.de>
2010-10-17 19:30:41 +0200

    added log output

--- vch-logger/src/main/java/de/berlios/vch/logger/Slf4jLogger.java
+++ vch-logger/src/main/java/de/berlios/vch/logger/Slf4jLogger.java
@@ -5,9 +5,12 @@ import org.apache.felix.ipojo.annotations.Invalidate;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.Validate;
 import org.osgi.service.log.LogReaderService;
+import org.osgi.service.log.LogService;
 
 @Component
 public class Slf4jLogger {
+    @Requires
+    private LogService logger;
     
     @Requires
     private LogReaderService logService;
@@ -16,6 +19,7 @@ public class Slf4jLogger {
     public void validate() {
         Slf4jLogListener logListener = new Slf4jLogListener();
         logService.addLogListener(logListener);
+        logger.log(LogService.LOG_INFO, "LogListener registered");
     }
     
     @Invalidate
