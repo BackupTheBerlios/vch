9eb52061fcad7e8ba740db95a665633fdb2ae1ca
Henrik Niehaus <henrik.niehaus@gmx.de>
2010-10-31 20:13:41 +0100

    comment

--- vch-parser/src/main/java/de/berlios/vch/parser/IParserService.java
+++ vch-parser/src/main/java/de/berlios/vch/parser/IParserService.java
@@ -3,7 +3,6 @@ package de.berlios.vch.parser;
 import java.net.URI;
 import java.util.Set;
 
-// TODO thread pool zum parsen bereitstellen, damit nicht jeder parser einen anlegen muss?
 public interface IParserService {
     public IWebPage parse(URI vchUri) throws Exception;
     
