668763a7267e95bb342e26ccdfcc70bc88e5252b
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-02-17 18:14:52 +0100

    added possibility to get a list of downloads with ajax requests. the reponse is json encoded.

--- vch-download/src/main/java/de/berlios/vch/download/webinterface/DownloadsServlet.java
+++ vch-download/src/main/java/de/berlios/vch/download/webinterface/DownloadsServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,8 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.log.LogService;
 
+import de.berlios.vch.download.Download;
 import de.berlios.vch.download.DownloadManager;
 import de.berlios.vch.download.PlaylistFileFoundException;
+import de.berlios.vch.download.jaxb.DownloadDTO;
 import de.berlios.vch.parser.IParserService;
 import de.berlios.vch.parser.IVideoPage;
 import de.berlios.vch.parser.IWebPage;
@@ -96,22 +99,62 @@ public class DownloadsServlet extends BundleContextServlet {
     }
 
     private void listDownloads(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("TITLE", i18n.translate("I18N_DOWNLOADS"));
-        params.put("ACTION", PATH);
-        params.put("FILE_PATH", FILE_PATH);
-        params.put("STATIC_PATH", STATIC_PATH);
-        params.put("DOWNLOADS", dm.getActiveDownloads());
-        params.put("FINISHED_DOWNLOADS", dm.getFinishedDownloads());
-        params.put("NOTIFY_MESSAGES", getNotifyMessages(req));
-        
-        // additional css
-        List<String> css = new ArrayList<String>();
-        css.add(STATIC_PATH + "/downloads.css");
-        params.put("CSS_INCLUDES", css);
-        
-        String page = templateLoader.loadTemplate("downloads.ftl", params);
-        resp.getWriter().print(page);
+        if ("XMLHttpRequest".equals(req.getHeader("X-Requested-With"))) {
+            // ajax request -> respond with json
+            resp.setContentType("application/json; charset=utf-8");
+            boolean active = "active".equals(req.getParameter("list"));
+            if(active) {
+                printActiveList(resp);
+            } else {
+                printFinishedList(resp);
+            }
+        } else {
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("TITLE", i18n.translate("I18N_DOWNLOADS"));
+            params.put("ACTION", PATH);
+            params.put("FILE_PATH", FILE_PATH);
+            params.put("STATIC_PATH", STATIC_PATH);
+            params.put("DOWNLOADS", dm.getActiveDownloads());
+            params.put("FINISHED_DOWNLOADS", dm.getFinishedDownloads());
+            params.put("NOTIFY_MESSAGES", getNotifyMessages(req));
+            
+            // additional css
+            List<String> css = new ArrayList<String>();
+            css.add(STATIC_PATH + "/downloads.css");
+            params.put("CSS_INCLUDES", css);
+            
+            String page = templateLoader.loadTemplate("downloads.ftl", params);
+            resp.getWriter().print(page);
+        }
+    }
+
+    private void printFinishedList(HttpServletResponse resp) throws IOException {
+        resp.getWriter().print("[");
+        for (Iterator<DownloadDTO> iterator = dm.getFinishedDownloads().iterator(); iterator.hasNext();) {
+            DownloadDTO download = iterator.next();
+            resp.getWriter().print("{\"id\":\"" + download.getId()+ "\"," 
+                    + "\"title\":\"" + download.getTitle().replaceAll("\"", "\\\\\"")+"\"}");
+            if(iterator.hasNext()) {
+                resp.getWriter().print(",");
+            }
+        }
+        resp.getWriter().print("]");
+    }
+
+    private void printActiveList(HttpServletResponse resp) throws IOException {
+        resp.getWriter().print("[");
+        for (Iterator<Download> iterator = dm.getActiveDownloads().iterator(); iterator.hasNext();) {
+            Download download = iterator.next();
+            resp.getWriter().print("{\"id\":\"" + download.getId()+ "\"," 
+                    + "\"title\":\"" + download.getVideoPage().getTitle().replaceAll("\"", "\\\\\"")+"\","
+                    + "\"progress\":" + download.getProgress()+","
+                    + "\"status\":\"" + download.getStatus()+"\","
+                    + "\"throughput\":" + download.getSpeed()+"}");
+            if(iterator.hasNext()) {
+                resp.getWriter().print(",");
+            }
+        }
+        resp.getWriter().print("]");
     }
 
     @Override
