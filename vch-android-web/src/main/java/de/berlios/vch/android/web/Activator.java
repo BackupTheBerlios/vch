6ced9ddb8a6e3a41c301c45ee37f9ce8d140e460
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-02-18 16:42:29 +0100

    vch-android-web is a download page for the VCH android app

---
+++ vch-android-web/src/main/java/de/berlios/vch/android/web/Activator.java
@@ -0,0 +1,129 @@
+package de.berlios.vch.android.web;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletException;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.osgi.service.log.LogService;
+
+import de.berlios.vch.i18n.Messages;
+import de.berlios.vch.i18n.ResourceBundleLoader;
+import de.berlios.vch.i18n.ResourceBundleProvider;
+import de.berlios.vch.web.ResourceHttpContext;
+import de.berlios.vch.web.TemplateLoader;
+import de.berlios.vch.web.menu.IWebMenuEntry;
+import de.berlios.vch.web.menu.WebMenuEntry;
+
+@Component
+public class Activator implements ResourceBundleProvider {
+
+    private BundleContext ctx;
+    
+    private ResourceBundle resourceBundle;
+    
+    @Requires
+    private LogService logger;
+    
+    @Requires
+    private Messages messages;
+    
+    @Requires
+    private TemplateLoader templateLoader;
+    
+    @Requires
+    private HttpService httpService;
+    
+    private List<ServiceRegistration> serviceRegs = new LinkedList<ServiceRegistration>();
+   
+    public Activator(BundleContext ctx) {
+        this.ctx = ctx;
+    }
+    
+    @Validate
+    public void start() {
+        messages.addProvider(this);
+        try {
+            // register downloads and configuration servlet
+            registerServlets();
+            
+            // register web interface menu
+            WebMenuEntry menu = new WebMenuEntry();
+            menu.setTitle("Android");
+            menu.setPreferredPosition(Integer.MAX_VALUE);
+            menu.setLinkUri(Servlet.PATH);
+            ServiceRegistration sr = ctx.registerService(IWebMenuEntry.class.getName(), menu, null);
+            serviceRegs.add(sr);
+        } catch (Exception e) {
+            logger.log(LogService.LOG_ERROR, "Couldn't start android web servlet", e);
+        }
+    }
+
+    @Invalidate
+    public void stop() {
+        // unregister the servlet
+        unregisterServlets();
+
+        // unregister web menu etc
+        for (Iterator<ServiceRegistration> iterator = serviceRegs.iterator(); iterator.hasNext();) {
+            ServiceRegistration sr = iterator.next();
+            unregisterService(sr);
+            iterator.remove();
+        }
+        
+        messages.removeProvider(this);
+    }
+    
+    private void unregisterService(ServiceRegistration sr) {
+        if (sr != null) {
+            sr.unregister();
+        }
+    }
+
+    private void unregisterServlets() {
+        if(httpService != null) {
+            httpService.unregister(Servlet.PATH);
+            httpService.unregister(Servlet.STATIC_PATH);
+        }
+    }
+
+    private void registerServlets() throws ServletException, NamespaceException {
+        Servlet servlet = new Servlet();
+        servlet.setBundleContext(ctx);
+        servlet.setMessages(messages);
+        servlet.setTemplateLoader(templateLoader);
+        servlet.setLogger(logger);
+        
+        // register servlet
+        httpService.registerServlet(Servlet.PATH, servlet, null, null);
+        
+        // register resource context for static files
+        ResourceHttpContext resourceHttpContext = new ResourceHttpContext(ctx, logger);
+        httpService.registerResources(Servlet.STATIC_PATH, "/htdocs", resourceHttpContext);
+    }
+
+    @Override
+    public ResourceBundle getResourceBundle() {
+        if(resourceBundle == null) {
+            try {
+                logger.log(LogService.LOG_DEBUG, "Loading resource bundle for " + getClass().getSimpleName());
+                resourceBundle = ResourceBundleLoader.load(ctx, Locale.getDefault());
+            } catch (IOException e) {
+                logger.log(LogService.LOG_ERROR, "Couldn't load resource bundle", e);
+            }
+        }
+        return resourceBundle;
+    }
+}
---
+++ vch-android-web/src/main/resources/templates/android.ftl
@@ -0,0 +1,19 @@
+<#include "header.ftl">
+<#include "status_messages.ftl">
+<#include "navigation.ftl">
+
+<h2>${TITLE}</h2>
+
+<img src="${STATIC_PATH}/qrcode.png" alt="QR-Code" style="float: left" />
+<div style="position: relative">
+<p style="width: 900px; position: relative; top: 1em">
+${I18N_SCANNER_HELP}
+<br/><br/>
+<a href="http://vch.berlios.de/repo/releases/de/berlios/vch/vchr/1.0.0/vchr-1.0.0.apk">Download</a>
+</p>
+</div>
+<p style="clear: both"><br/><br/>${I18N_HELP}</p>
+<img src="${STATIC_PATH}/help/settings_root.png" alt="${I18N_ALT_SETTINGS}"/>
+<img src="${STATIC_PATH}/help/settings_applications.png" alt="${I18N_ALT_SETTINGS_APPS}"/>
+
+<#include "footer.ftl">
