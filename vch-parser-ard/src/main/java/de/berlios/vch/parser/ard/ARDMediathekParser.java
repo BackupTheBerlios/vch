2c82bce273b4e2c0cd9c7f7c4b2043216e977579
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-02-20 20:07:43 +0100

    use ipojo instead of plain osgi api

--- vch-parser-ard/src/main/java/de/berlios/vch/parser/ard/ARDMediathekParser.java
+++ vch-parser-ard/src/main/java/de/berlios/vch/parser/ard/ARDMediathekParser.java
@@ -4,10 +4,11 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Provides;
 import org.htmlparser.util.Translate;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 import de.berlios.vch.http.client.HttpUtils;
@@ -18,7 +19,9 @@ import de.berlios.vch.parser.IWebParser;
 import de.berlios.vch.parser.OverviewPage;
 import de.berlios.vch.parser.VideoPage;
 
-public class ARDMediathekParser implements IWebParser, BundleActivator {
+@Component
+@Provides
+public class ARDMediathekParser implements IWebParser {
 
     public static final String BASE_URL = "http://www.ardmediathek.de";
     
@@ -37,6 +40,10 @@ public class ARDMediathekParser implements IWebParser, BundleActivator {
         HTTP_HEADERS.put("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090821 Gentoo Firefox/3.5.7");
         HTTP_HEADERS.put("Accept-Language", "de-de,de;q=0.8,en-us;q=0.5,en;q=0.3");
     }
+    
+    public ARDMediathekParser(BundleContext ctx) {
+    	this.ctx = ctx;
+	}
     
     @Override
     public IOverviewPage getRoot() throws Exception {
@@ -83,16 +90,6 @@ public class ARDMediathekParser implements IWebParser, BundleActivator {
             page = VideoItemPageParser.parse((VideoPage) page, ctx);
         }
         return page;
-    }
-
-    @Override
-    public void start(BundleContext ctx) throws Exception {
-        this.ctx = ctx;
-        ctx.registerService(IWebParser.class.getName(), this, null);
-    }
-
-    @Override
-    public void stop(BundleContext ctx) throws Exception {
     }
     
     @Override
