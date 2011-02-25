2a6a17efa60bea29a7f89962dff8abec75f53fe6
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-02-25 19:03:10 +0100

    code formatting

--- vch-parser-arte/src/main/java/de/berlios/vch/parser/arte/ArteParser.java
+++ vch-parser-arte/src/main/java/de/berlios/vch/parser/arte/ArteParser.java
@@ -35,34 +35,35 @@ import de.berlios.vch.parser.VideoPage;
 @Component
 @Provides
 public class ArteParser implements IWebParser {
-    
+
     public static final String CHARSET = "UTF-8";
-    
+
     public static final String ARTE_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-    
+
     public static final String BASE_URI = "http://videos.arte.tv";
-    
+
     public static final String START_PAGE = BASE_URI + "/de/videos/sendungen";
-    
+
     public static final String ID = ArteParser.class.getName();
-    
+
     @Requires
     private LogService logger;
-    
+
     private BundleContext ctx;
-    
+
     private VideoPageParser videoPageParser;
-    
-    public static Map<String,String> HTTP_HEADERS = new HashMap<String, String>();
+
+    public static Map<String, String> HTTP_HEADERS = new HashMap<String, String>();
     static {
-        HTTP_HEADERS.put("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090821 Gentoo Firefox/3.5.2");
+        HTTP_HEADERS.put("User-Agent",
+                "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090821 Gentoo Firefox/3.5.2");
         HTTP_HEADERS.put("Accept-Language", "de-de,de;q=0.8,en-us;q=0.5,en;q=0.3");
     }
-    
-    public ArteParser(BundleContext ctx) {;
+
+    public ArteParser(BundleContext ctx) {
         this.ctx = ctx;
     }
-    
+
     @Override
     public IOverviewPage getRoot() throws Exception {
         OverviewPage page = new OverviewPage();
@@ -72,7 +73,7 @@ public class ArteParser implements IWebParser {
         parsePrograms(page);
         return page;
     }
-    
+
     private void parsePrograms(OverviewPage root) throws IOException, ParserException, URISyntaxException {
         String content = HttpUtils.get(START_PAGE, HTTP_HEADERS, CHARSET);
         NodeList programs = HtmlParserUtils.getTags(content, CHARSET, "div.newVideos h1 > a");
@@ -83,7 +84,7 @@ public class ArteParser implements IWebParser {
             opage.setTitle(Translate.decode(link.getLinkText()));
             opage.setUri(new URI(BASE_URI + link.getLink()));
             root.getPages().add(opage);
-            
+
             // skip the second matching link for each program
             iterator.nextNode();
         }
@@ -96,13 +97,11 @@ public class ArteParser implements IWebParser {
 
     @Override
     public IWebPage parse(IWebPage page) throws Exception {
-        if(page instanceof IVideoPage) {
+        if (page instanceof IVideoPage) {
             IVideoPage video = (IVideoPage) page;
-            if(video.getVideoUri() == null) {
-                videoPageParser.parse(video);
-            }
+            videoPageParser.parse(video);
             return video;
-        } else if(page instanceof IOverviewPage) {
+        } else if (page instanceof IOverviewPage) {
             IOverviewPage opage = (IOverviewPage) page;
             parseBroadcasts(opage);
             return page;
@@ -110,7 +109,7 @@ public class ArteParser implements IWebParser {
             return page;
         }
     }
-    
+
     private void parseBroadcasts(IOverviewPage opage) throws Exception {
         String uri = opage.getUri().toString() + "#/de/list///1/150/";
         String content = HttpUtils.get(uri, HTTP_HEADERS, CHARSET);
@@ -123,21 +122,22 @@ public class ArteParser implements IWebParser {
             video.setParser(getId());
 
             // parse title and page uri
-            LinkTag link = (LinkTag)HtmlParserUtils.getTag(nodeContent, CHARSET, "h2 a");
+            LinkTag link = (LinkTag) HtmlParserUtils.getTag(nodeContent, CHARSET, "h2 a");
             video.setTitle(Translate.decode(link.getLinkText()));
             video.setUri(new URI(BASE_URI + link.getLink()));
-            
+
             // parse description
             video.setDescription(HtmlParserUtils.getText(nodeContent, CHARSET, "p.teaserText"));
-            
+
             // parse thumbnail
             ImageTag thumb = (ImageTag) HtmlParserUtils.getTag(nodeContent, CHARSET, "img.thumbnail");
             video.setThumbnail(new URI(BASE_URI + thumb.getImageURL()));
-            
+
             // parse date (Di, 13. Apr 2010, 00:00)
             try {
                 String format = "EE, dd. MMM yyyy, HH:mm";
-                Node ps = HtmlParserUtils.getTag(nodeContent, CHARSET, "p.views").getPreviousSibling().getPreviousSibling();
+                Node ps = HtmlParserUtils.getTag(nodeContent, CHARSET, "p.views").getPreviousSibling()
+                        .getPreviousSibling();
                 String dateString = ps.getFirstChild().getText();
                 SimpleDateFormat sdf = new SimpleDateFormat(format);
                 Calendar pubDate = Calendar.getInstance();
@@ -146,7 +146,7 @@ public class ArteParser implements IWebParser {
             } catch (Exception e) {
                 logger.log(LogService.LOG_WARNING, "Couldn't parse publish date", e);
             }
-            
+
             opage.getPages().add(video);
         }
     }
@@ -157,7 +157,8 @@ public class ArteParser implements IWebParser {
     }
 
     @Invalidate
-    public void stop()  {}
+    public void stop() {
+    }
 
     @Override
     public String getId() {
