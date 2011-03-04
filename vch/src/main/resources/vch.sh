13bcfb72ff91c0a919a99954c669fa21561d7e8e
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-03-04 14:03:04 +0100

    fixed classpath entry of the logging jar

--- vch/src/main/resources/vch.sh
+++ vch/src/main/resources/vch.sh
@@ -4,11 +4,11 @@ cd `dirname $0`
 
 java -Djava.net.preferIPv4Stack=true \
      -Djava.util.logging.config.file=conf/logging.properties \
-     -cp bundles/slf4j-logger.jar:bin/org.apache.felix.main-${felix.main.version}.jar \
+     -cp bundles/logger.jar:bin/org.apache.felix.main-${felix.main.version}.jar \
      org.apache.felix.main.Main
      
 #java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 \
 #     -Djava.net.preferIPv4Stack=true \
 #     -Djava.util.logging.config.file=conf/logging.properties \
-#     -cp bundles/slf4j-logger.jar:bin/org.apache.felix.main-${felix.main.version}.jar \
+#     -cp bundles/logger.jar:bin/org.apache.felix.main-${felix.main.version}.jar \
 #     org.apache.felix.main.Main
