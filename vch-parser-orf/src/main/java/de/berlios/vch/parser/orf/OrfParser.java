74a950f0cc65d050eef491a64809610b99871524
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-03-04 18:39:46 +0100

    improved the video page parser

--- vch-parser-orf/src/main/java/de/berlios/vch/parser/orf/OrfParser.java
+++ vch-parser-orf/src/main/java/de/berlios/vch/parser/orf/OrfParser.java
@@ -42,10 +42,10 @@ import de.berlios.vch.parser.VideoPage;
 public class OrfParser implements IWebParser {
 
     public static final String ID = OrfParser.class.getName();
-    
+
     protected static final String BASE_URI = "http://tvthek.orf.at";
     protected static final String CHARSET = "UTF-8";
-    
+
     @Requires
     private LogService logger;
 
@@ -55,12 +55,12 @@ public class OrfParser implements IWebParser {
         page.setParser(ID);
         page.setTitle(getTitle());
         page.setUri(new URI("vchpage://localhost/" + getId()));
-        
+
         String content = HttpUtils.get(BASE_URI, null, CHARSET);
         SelectTag select = (SelectTag) HtmlParserUtils.getTag(content, CHARSET, "form#programs select");
         for (OptionTag option : select.getOptionTags()) {
             String uri = option.getValue();
-            if(uri.startsWith("http://")) {
+            if (uri.startsWith("http://")) {
                 String name = Translate.decode(option.toPlainTextString().trim());
                 IOverviewPage opage = new OverviewPage();
                 opage.setParser(ID);
@@ -69,7 +69,7 @@ public class OrfParser implements IWebParser {
                 page.getPages().add(opage);
             }
         }
-        
+
         return page;
     }
 
@@ -80,54 +80,54 @@ public class OrfParser implements IWebParser {
 
     @Override
     public IWebPage parse(IWebPage page) throws Exception {
-        if(page instanceof IOverviewPage) {
-            if("dummy".equals(page.getUri().getScheme())) {
+        if (page instanceof IOverviewPage) {
+            if ("dummy".equals(page.getUri().getScheme())) {
                 return page;
             }
-            
+
             return parseOverviewPage(page);
-        } else if(page instanceof IVideoPage) {
+        } else if (page instanceof IVideoPage) {
             return parseVideoPage(page);
         } else {
             return page;
         }
     }
-    
+
     private IWebPage parseOverviewPage(IWebPage page) throws Exception {
         IOverviewPage opage = (IOverviewPage) page;
         String content = HttpUtils.get(page.getUri().toString(), null, CHARSET);
-        
+
         // parse segments
         NodeList segments = HtmlParserUtils.getTags(content, CHARSET, "div#segment-tab ul.vods > li");
-        if(segments.size() > 0) {
+        if (segments.size() > 0) {
             logger.log(LogService.LOG_INFO, "Segmente: " + segments.size());
             for (NodeIterator iterator = segments.elements(); iterator.hasMoreNodes();) {
                 Bullet li = (Bullet) iterator.nextNode();
                 LinkTag a = (LinkTag) HtmlParserUtils.findChildByType(li, LinkTag.class);
-                if(a != null) {
+                if (a != null) {
                     IVideoPage video = new VideoPage();
                     video.setParser(getId());
                     video.setTitle(a.getAttribute("title"));
                     video.setUri(new URI(a.getLink()));
                     logger.log(LogService.LOG_INFO, "Segment page is " + video.getUri());
-                    
+
                     // parse the duration
                     try {
                         Span span = (Span) HtmlParserUtils.findChildByType(a, Span.class);
-                        if(span != null) {
-                            //(00:35)
+                        if (span != null) {
+                            // (00:35)
                             String text = span.getStringText();
                             logger.log(LogService.LOG_INFO, "Duration: " + text);
-                            text = text.substring(1, text.length()-1);
+                            text = text.substring(1, text.length() - 1);
                             int minutes = Integer.parseInt(text.substring(0, text.indexOf(':')));
-                            int seconds = Integer.parseInt(text.substring(text.indexOf(':')+1, text.length()));
+                            int seconds = Integer.parseInt(text.substring(text.indexOf(':') + 1, text.length()));
                             int duration = minutes * 60 + seconds;
                             video.setDuration(duration);
                         }
-                    } catch(Exception e) {
+                    } catch (Exception e) {
                         logger.log(LogService.LOG_WARNING, "Couldn't parse segment duration", e);
                     }
-                    
+
                     opage.getPages().add(video);
                 }
                 logger.log(LogService.LOG_INFO, "Tag: " + li.getClass().getName());
@@ -142,10 +142,10 @@ public class OrfParser implements IWebParser {
             video.setUri(page.getUri());
             opage.getPages().add(video);
         }
-        
+
         // parse more episodes
         NodeList dayList = HtmlParserUtils.getTags(content, CHARSET, "div#more-episodes div.scrollbox > ul > li");
-        if(dayList.size() > 0) {
+        if (dayList.size() > 0) {
             for (NodeIterator iterator = dayList.elements(); iterator.hasMoreNodes();) {
                 // add the day
                 Tag li = (Tag) iterator.nextNode();
@@ -155,7 +155,7 @@ public class OrfParser implements IWebParser {
                 day.setTitle(title);
                 day.setUri(new URI("dummy://" + UUID.randomUUID()));
                 opage.getPages().add(day);
-                
+
                 // add episodes for the day
                 NodeList episodeList = HtmlParserUtils.getTags(li.toHtml(), CHARSET, "ul li");
                 for (NodeIterator epIter = episodeList.elements(); epIter.hasMoreNodes();) {
@@ -170,39 +170,51 @@ public class OrfParser implements IWebParser {
                 }
             }
         }
-        
+
         return page;
     }
 
     private IVideoPage parseVideoPage(IWebPage page) throws ParserException, IOException, URISyntaxException {
         IVideoPage vpage = (IVideoPage) page;
         String content = HttpUtils.get(page.getUri().toString(), null, CHARSET);
-        
+
         // parse the video uri
         Tag param = HtmlParserUtils.getTag(content, CHARSET, "param[name=URL]");
         String videoUri = BASE_URI + param.getAttribute("value");
-        if(videoUri.toLowerCase().endsWith(".asx")) {
+        if (videoUri.toLowerCase().endsWith(".asx")) {
             videoUri = AsxParser.getUri(videoUri);
         }
         vpage.setVideoUri(new URI(videoUri));
-        
+
         // parse the title
-        String title = HtmlParserUtils.getText(content, CHARSET, "h3.title span");
-        //vpage.setTitle(title);
-        
+        String h3 = HtmlParserUtils.getText(content, CHARSET, "h3.title span");
+        String title = h3;
+        if (title.contains(" - ")) {
+            title = title.substring(0, title.indexOf(" - "));
+        }
+        LinkTag a = (LinkTag) HtmlParserUtils.getTag(content, CHARSET, "ul#segments_playlist li.active a");
+        if (a != null) {
+            title += " - " + Translate.decode(a.getAttribute("title"));
+        }
+        vpage.setTitle(title);
+
         // parse description if available
         ParagraphTag desc = (ParagraphTag) HtmlParserUtils.getTag(content, CHARSET, "div#info-tab div.content p");
-        if(desc != null) {
+        if (desc != null) {
             vpage.setDescription(Translate.decode(desc.toPlainTextString().trim()));
+        } else {
+            // try to parse the description of the segments list
+            String _desc = HtmlParserUtils.getText(content, CHARSET, "ul#segments_playlist li.active p");
+            vpage.setDescription(_desc);
         }
-        
+
         // parse the pubdate
         String datePattern = "dd.MM.yyyy";
-        Matcher m = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4})").matcher(title);
-        if(m.find()) {
+        Matcher m = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4})").matcher(h3);
+        if (m.find()) {
             String date = m.group(1);
             logger.log(LogService.LOG_DEBUG, "PubDate is " + date);
-            
+
             try {
                 Date pubDate = new SimpleDateFormat(datePattern).parse(date);
                 Calendar cal = Calendar.getInstance();
@@ -212,7 +224,21 @@ public class OrfParser implements IWebParser {
                 logger.log(LogService.LOG_WARNING, "Couldn't parse pubDate " + date + " with pattern " + datePattern);
             }
         }
-        
+
+        // parse the duration
+        String duration = HtmlParserUtils.getText(content, CHARSET, "ul#segments_playlist li.active span.duration");
+        if (!duration.isEmpty()) {
+            try {
+                duration = duration.substring(1, duration.length() - 1);
+                int pos = duration.indexOf(':');
+                int minutes = Integer.parseInt(duration.substring(0, pos));
+                int secs = Integer.parseInt(duration.substring(pos + 1));
+                vpage.setDuration(minutes * 60 + secs);
+            } catch (Exception e) {
+                logger.log(LogService.LOG_WARNING, "Couldn't parse the duration " + duration, e);
+            }
+        }
+
         return vpage;
     }
 
