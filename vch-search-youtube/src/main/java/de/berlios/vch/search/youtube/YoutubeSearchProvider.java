e5fb3dd1a864f1b1b8741878b055ca90e5add9ee
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-03-07 19:16:18 +0100

    changed ipojo instance name

--- vch-search-youtube/src/main/java/de/berlios/vch/search/youtube/YoutubeSearchProvider.java
+++ vch-search-youtube/src/main/java/de/berlios/vch/search/youtube/YoutubeSearchProvider.java
@@ -23,57 +23,56 @@ import de.berlios.vch.search.ISearchProvider;
 @Provides
 public class YoutubeSearchProvider implements ISearchProvider {
 
-	@Requires(filter="(instance.name=VCH Youtube Parser)")
-	private IWebParser youtubeParser;
-	
-	@Override
-	public String getName() {
-		return youtubeParser.getTitle();
-	}
-	
-	@Override
-	public IOverviewPage search(String query) throws Exception {
-		if(youtubeParser == null) {
-			throw new ServiceException("Youtube Parser is not available");
-		}
-		
-		String uri = "http://gdata.youtube.com/feeds/base/videos?q="
-			+ URLEncoder.encode(query, "UTF-8") 
-			+ "&client=ytapi-youtube-search&alt=rss&v=2";
-		IOverviewPage opage = new OverviewPage();
-		opage.setParser(getId());
-		opage.setUri(new URI(uri));
-		opage = (IOverviewPage)youtubeParser.parse(opage);
-		
-		// remove related videos and keep only the IVideoPages
-		List<IVideoPage> videos = new ArrayList<IVideoPage>(opage.getPages().size());
-		for (IWebPage _subpage : opage.getPages()) {
-			IOverviewPage subpage = (IOverviewPage) _subpage;
-			videos.add((IVideoPage) subpage.getPages().get(0));
-		}
-		opage.getPages().clear();
-		opage.getPages().addAll(videos);
-		return opage;
-	}
+    @Requires(filter = "(instance.name=vch.parser.youtube)")
+    private IWebParser youtubeParser;
 
-	@Override
-	public String getId() {
-		return youtubeParser.getId();
-	}
+    @Override
+    public String getName() {
+        return youtubeParser.getTitle();
+    }
 
-	@Override
-	public IWebPage parse(IWebPage page) throws Exception {
-		if(page instanceof IVideoPage) {
-			Map<String, List<String>> params = HttpUtils.parseQuery(page.getUri().getQuery());
-			String youtubeId = params.get("v").get(0);
-			IOverviewPage opageProxy = (IOverviewPage) youtubeParser.parse(search(youtubeId));
-			IOverviewPage opage = (IOverviewPage) opageProxy.getPages().get(0);
-			return opage.getPages().get(0);
-		} else { // TODO is this block still needed?
-			if(youtubeParser == null) {
-				throw new ServiceException("Youtube Parser is not available");
-			}
-			return youtubeParser.parse(page);
-		}
-	}
+    @Override
+    public IOverviewPage search(String query) throws Exception {
+        if (youtubeParser == null) {
+            throw new ServiceException("Youtube Parser is not available");
+        }
+
+        String uri = "http://gdata.youtube.com/feeds/base/videos?q=" + URLEncoder.encode(query, "UTF-8")
+                + "&client=ytapi-youtube-search&alt=rss&v=2";
+        IOverviewPage opage = new OverviewPage();
+        opage.setParser(getId());
+        opage.setUri(new URI(uri));
+        opage = (IOverviewPage) youtubeParser.parse(opage);
+
+        // remove related videos and keep only the IVideoPages
+        List<IVideoPage> videos = new ArrayList<IVideoPage>(opage.getPages().size());
+        for (IWebPage _subpage : opage.getPages()) {
+            IOverviewPage subpage = (IOverviewPage) _subpage;
+            videos.add((IVideoPage) subpage.getPages().get(0));
+        }
+        opage.getPages().clear();
+        opage.getPages().addAll(videos);
+        return opage;
+    }
+
+    @Override
+    public String getId() {
+        return youtubeParser.getId();
+    }
+
+    @Override
+    public IWebPage parse(IWebPage page) throws Exception {
+        if (page instanceof IVideoPage) {
+            Map<String, List<String>> params = HttpUtils.parseQuery(page.getUri().getQuery());
+            String youtubeId = params.get("v").get(0);
+            IOverviewPage opageProxy = (IOverviewPage) youtubeParser.parse(search(youtubeId));
+            IOverviewPage opage = (IOverviewPage) opageProxy.getPages().get(0);
+            return opage.getPages().get(0);
+        } else { // TODO is this block still needed?
+            if (youtubeParser == null) {
+                throw new ServiceException("Youtube Parser is not available");
+            }
+            return youtubeParser.parse(page);
+        }
+    }
 }
