a1b3af4a878673c9d0a8f0c3833f63d679dc45dc
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-03-07 18:32:51 +0100

    use ipojo for the servlets

--- vch-osdserver/src/main/java/de/berlios/vch/osdserver/Activator.java
+++ vch-osdserver/src/main/java/de/berlios/vch/osdserver/Activator.java
@@ -3,9 +3,6 @@ package de.berlios.vch.osdserver;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.prefs.Preferences;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Invalidate;
@@ -13,20 +10,12 @@ import org.apache.felix.ipojo.annotations.Provides;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.Validate;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.http.HttpService;
 import org.osgi.service.log.LogService;
 import org.osgi.util.tracker.ServiceTracker;
 
-import de.berlios.vch.config.ConfigService;
-import de.berlios.vch.i18n.Messages;
 import de.berlios.vch.i18n.ResourceBundleLoader;
 import de.berlios.vch.i18n.ResourceBundleProvider;
 import de.berlios.vch.parser.service.IParserService;
-import de.berlios.vch.playlist.PlaylistService;
-import de.berlios.vch.web.TemplateLoader;
-import de.berlios.vch.web.menu.IWebMenuEntry;
-import de.berlios.vch.web.menu.WebMenuEntry;
 
 @Component
 @Provides
@@ -39,24 +28,7 @@ public class Activator implements ResourceBundleProvider {
 
     private BundleContext ctx;
 
-    @Requires
-    private Messages i18n;
-
-    @Requires
-    private HttpService httpService;
-
-    @Requires
-    private ConfigService cs;
-    
-    @Requires
-    private TemplateLoader templateLoader;
-    
-    @Requires
-    private PlaylistService playlistService;
-
     private ResourceBundle resourceBundle;
-    
-    private ServiceRegistration menuReg;
 
     public Activator(BundleContext ctx) {
         this.ctx = ctx;
@@ -66,57 +38,12 @@ public class Activator implements ResourceBundleProvider {
     public void start() {
         parserServiceTracker = new ServiceTracker(ctx, IParserService.class.getName(), null);
         parserServiceTracker.open();
-
-        try {
-            httpService.registerServlet("/osdserver", new ActivatorServlet(ctx, i18n, playlistService), null, null);
-            registerConfigServlet();
-        } catch (Exception e) {
-            logger.log(LogService.LOG_ERROR, "Couldn't register osdserver startup servlet", e);
-        }
-    }
-
-    private void registerConfigServlet() {
-        Preferences prefs = cs.getUserPreferences(ctx.getBundle().getSymbolicName());
-        ConfigServlet servlet = new ConfigServlet(prefs);
-        servlet.setLogger(logger);
-        servlet.setBundleContext(ctx);
-        servlet.setMessages(i18n);
-        servlet.setTemplateLoader(templateLoader);
-        try {
-            // register the servlet
-            httpService.registerServlet(ConfigServlet.PATH, servlet, null, null);
-
-            // register web interface menu
-            IWebMenuEntry menu = new WebMenuEntry(getResourceBundle().getString("I18N_CONFIGURATION"));
-            menu.setPreferredPosition(Integer.MAX_VALUE - 1);
-            menu.setLinkUri("#");
-            SortedSet<IWebMenuEntry> childs = new TreeSet<IWebMenuEntry>();
-            IWebMenuEntry entry = new WebMenuEntry();
-            entry.setTitle("Osdserver");
-            entry.setLinkUri(ConfigServlet.PATH);
-            childs.add(entry);
-            menu.setChilds(childs);
-            menuReg = ctx.registerService(IWebMenuEntry.class.getName(), menu, null);
-        } catch (Exception e) {
-            logger.log(LogService.LOG_ERROR, "Couldn't register osdserver config servlet", e);
-        }
     }
 
     @Invalidate
     public void stop() {
         parserServiceTracker.close();
         parserServiceTracker = null;
-        
-        // unregister the config servlet
-        if(httpService != null) {
-            httpService.unregister("/osdserver");
-            httpService.unregister(ConfigServlet.PATH);
-        }
-        
-        // unregister the web menu
-        if(menuReg != null) {
-            menuReg.unregister();
-        }
     }
 
     @Override
--- vch-osdserver/src/main/java/de/berlios/vch/osdserver/ActivatorServlet.java
+++ vch-osdserver/src/main/java/de/berlios/vch/osdserver/ActivatorServlet.java
@@ -6,45 +6,77 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 
 import de.berlios.vch.i18n.Messages;
 import de.berlios.vch.playlist.PlaylistService;
+import de.berlios.vch.web.servlets.VchHttpServlet;
 
-public class ActivatorServlet extends HttpServlet {
+@Component
+public class ActivatorServlet extends VchHttpServlet {
 
+    public final static String PATH = "/osdserver";
+
+    @Requires
     private Messages i18n;
-    
-    private BundleContext ctx;
-    
+
+    @Requires
     private PlaylistService playlistService;
-    
-    public ActivatorServlet(BundleContext ctx, Messages i18n, PlaylistService playlistService) {
-        this.i18n = i18n;
+
+    @Requires
+    private HttpService httpService;
+
+    private BundleContext ctx;
+
+    public ActivatorServlet(BundleContext ctx) {
         this.ctx = ctx;
-        this.playlistService = playlistService;
     }
-  
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	Map<String, String> requestPrefs = new HashMap<String, String>();
-    	Enumeration<?> prefNames = req.getParameterNames();
-    	while (prefNames.hasMoreElements()) {
-    		String name = (String)prefNames.nextElement();
-    		if (name != null) {
-        		String v = req.getParameter(name);
-        		if (v != null)
-        			requestPrefs.put(name, v);
-    		}
-    	}
-    	
-    	Thread t = new Thread(new OsdSession(ctx, i18n, playlistService, requestPrefs));
+    protected void get(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String> requestPrefs = new HashMap<String, String>();
+        Enumeration<?> prefNames = req.getParameterNames();
+        while (prefNames.hasMoreElements()) {
+            String name = (String) prefNames.nextElement();
+            if (name != null) {
+                String v = req.getParameter(name);
+                if (v != null) {
+                    requestPrefs.put(name, v);
+                }
+            }
+        }
+
+        Thread t = new Thread(new OsdSession(ctx, i18n, playlistService, requestPrefs));
         t.setName("Osdserver Session");
         t.start();
         resp.getWriter().println("Osdserver session started");
+    }
+
+    @Override
+    protected void post(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        get(req, resp);
+    }
+
+    @Validate
+    public void start() throws ServletException, NamespaceException {
+        // register the servlet
+        httpService.registerServlet(ActivatorServlet.PATH, this, null, null);
+    }
+
+    @Invalidate
+    public void stop() {
+        // unregister the config servlet
+        if (httpService != null) {
+            httpService.unregister(ActivatorServlet.PATH);
+        }
     }
 }
