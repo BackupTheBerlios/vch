b59c21a264b0cccc4155594b469b3b305dc305a9
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-03-09 13:14:26 +0100

    added news functionality, which shows news regarding VCH from hampelratte.org

--- vch-web/src/main/java/de/berlios/vch/web/servlets/WelcomeServlet.java
+++ vch-web/src/main/java/de/berlios/vch/web/servlets/WelcomeServlet.java
@@ -1,7 +1,10 @@
 package de.berlios.vch.web.servlets;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -12,8 +15,15 @@ import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Invalidate;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.Validate;
+import org.json.JSONObject;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 import de.berlios.vch.i18n.ResourceBundleProvider;
 import de.berlios.vch.web.TemplateLoader;
@@ -23,28 +33,55 @@ public class WelcomeServlet extends VchHttpServlet {
 
     public static final String PATH = "/vch";
 
-    @Requires(filter="(instance.name=vch.web)")
+    @Requires(filter = "(instance.name=vch.web)")
     private ResourceBundleProvider rbp;
-    
+
     @Requires
     private TemplateLoader templateLoader;
-    
+
     @Requires
     private HttpService httpService;
-    
+
     @Override
     protected void get(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("TITLE", rbp.getResourceBundle().getString("I18N_WELCOME"));
-        String page = templateLoader.loadTemplate("welcome.ftl", params);
-        resp.getWriter().print(page);
+        if (req.getParameter("news") != null) {
+            // load news
+            try {
+                SyndFeed feed = loadNewsFeed();
+                resp.setContentType("application/json; charset=utf-8");
+                resp.getWriter().print('[');
+                for (Iterator<?> iterator = feed.getEntries().iterator(); iterator.hasNext();) {
+                    SyndEntry entry = (SyndEntry) iterator.next();
+                    if (entry.getCategories().contains("VCH")) {
+                        JSONObject json = new JSONObject();
+                        json.put("title", entry.getTitle());
+                        json.put("date", entry.getPublishedDate());
+                        json.put("text", entry.getDescription().getValue());
+                        json.put("link", entry.getLink());
+                        resp.getWriter().print(json.toString());
+                        if (iterator.hasNext()) {
+                            resp.getWriter().print(',');
+                        }
+                    }
+                }
+                resp.getWriter().print(']');
+            } catch (Exception e) {
+                throw new ServletException("Couldn't load news feed", e);
+            }
+        } else {
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("TITLE", rbp.getResourceBundle().getString("I18N_WELCOME"));
+            params.put("SERVLET_URI", PATH);
+            String page = templateLoader.loadTemplate("welcome.ftl", params);
+            resp.getWriter().print(page);
+        }
     }
 
     @Override
     protected void post(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         get(req, resp);
     }
-    
+
     @Validate
     public void start() throws ServletException, NamespaceException {
         // register the servlet
@@ -55,6 +92,12 @@ public class WelcomeServlet extends VchHttpServlet {
     public void stop() {
         // unregister the servlet
         httpService.unregister(PATH);
+    }
+
+    private SyndFeed loadNewsFeed() throws IllegalArgumentException, MalformedURLException, FeedException, IOException {
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build(new XmlReader(new URL("http://www.hampelratte.org/blog/?feed=rss2")));
+        return feed;
     }
 
 }
--- vch-web/src/main/resources/templates/welcome.ftl
+++ vch-web/src/main/resources/templates/welcome.ftl
@@ -4,4 +4,52 @@
 
 <h2>${TITLE}</h2>
 
+<div id="news">${I18N_LOADING_NEWS} <img src="/static/icons/loadingAnimation.gif" alt=""/></div>
+<script type="text/javascript">
+<!--
+$(document).ready(function() {
+
+$.ajax({
+    url: '${SERVLET_URI}',
+    type: 'GET',
+    data: {
+        news : 'show'
+    },
+    dataType: 'json',
+    timeout: 30000,
+    success: function(response, status, xhr) {
+        $('#news').hide();
+        if(response.length == 0) {
+            $('#news').html('<p>${I18N_NO_NEWS}</p>').show();
+        } else {
+            $('#news').html('<ul></ul>');
+            for(var i=0; i<response.length; i++) {
+                var item = response[i];
+    
+                var li = document.createElement('li');
+                $(li).append('<h3>'+item.title+'</h3>');
+                $(li).append(' - '+item.date);
+                $(li).append('<p>'+item.text);
+                $(li).append(' <a href="'+item.link+'">${I18N_READ_MORE}</a></p>');
+                
+                $('#news ul').append($(li));    
+            }
+            $('#news').slideDown('slow');
+        }
+    },
+    error: function(xhr, statusText, error) {
+        $('#news').hide();
+        $.pnotify( {
+            pnotify_title : '${I18N_ERROR}',
+            pnotify_text : '${I18N_LOADING_NEWS_FAILED}<br/>' + error,
+            pnotify_type : 'error',
+            pnotify_hide: false
+        });
+    }
+});
+
+});
+-->
+</script>
+
 <#include "footer.ftl">
