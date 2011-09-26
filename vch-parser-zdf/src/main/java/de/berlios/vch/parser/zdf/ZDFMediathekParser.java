d1d84c7392c75fdf5882f97cf3551308a07e87e9
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-09-26 13:40:58 +0200

    code formatting

--- vch-parser-zdf/src/main/java/de/berlios/vch/parser/zdf/ZDFMediathekParser.java
+++ vch-parser-zdf/src/main/java/de/berlios/vch/parser/zdf/ZDFMediathekParser.java
@@ -41,15 +41,15 @@ public class ZDFMediathekParser implements IWebParser {
     public static final String ID = ZDFMediathekParser.class.getName();
     private static final String BASE_URI = "http://www.zdf.de";
     private static final String OVERVIEW_RUBRIKEN = BASE_URI + "/ZDFmediathek/hauptnavigation/rubriken?flash=off";
-    private static final String OVERVIEW_ABZ = "dummy://localhost/"+ID+"/a-z";
-    
+    private static final String OVERVIEW_ABZ = "dummy://localhost/" + ID + "/a-z";
+
     public static final String CHARSET = "UTF-8";
-    
+
     private Map<String, String> aBisZ = new TreeMap<String, String>();
-    
+
     @Requires
     private LogService logger;
-    
+
     public ZDFMediathekParser() {
         aBisZ.put("0-9", "http://www.zdf.de/ZDFmediathek/hauptnavigation/sendung-a-bis-z/saz8?flash=off&teaserListIndex=1000");
         aBisZ.put("ABC", "http://www.zdf.de/ZDFmediathek/hauptnavigation/sendung-a-bis-z/saz0?flash=off&teaserListIndex=1000");
@@ -61,26 +61,26 @@ public class ZDFMediathekParser implements IWebParser {
         aBisZ.put("TUV", "http://www.zdf.de/ZDFmediathek/hauptnavigation/sendung-a-bis-z/saz6?flash=off&teaserListIndex=1000");
         aBisZ.put("WXYZ", "http://www.zdf.de/ZDFmediathek/hauptnavigation/sendung-a-bis-z/saz7?flash=off&teaserListIndex=1000");
     }
-    
+
     @Override
     public IOverviewPage getRoot() throws Exception {
         OverviewPage page = new OverviewPage();
         page.setParser(ID);
         page.setTitle(getTitle());
         page.setUri(new URI("vchpage://localhost/" + getId()));
-        
+
         OverviewPage rubriken = new OverviewPage();
         rubriken.setParser(ID);
         rubriken.setTitle("Rubriken");
         rubriken.setUri(new URI(OVERVIEW_RUBRIKEN));
         page.getPages().add(rubriken);
-        
+
         OverviewPage abz = new OverviewPage();
         abz.setTitle("Sendungen A-Z");
         abz.setParser(ID);
         abz.setUri(new URI(OVERVIEW_ABZ));
         page.getPages().add(abz);
-        
+
         return page;
     }
 
@@ -91,20 +91,20 @@ public class ZDFMediathekParser implements IWebParser {
 
     @Override
     public IWebPage parse(IWebPage page) throws Exception {
-        if(page instanceof IVideoPage) {
+        if (page instanceof IVideoPage) {
             IVideoPage video = (IVideoPage) page;
             parseVideoPage(video);
             // parse asx file, if necessary
-            if(video.getVideoUri().toString().toLowerCase().endsWith(".asx")) {
+            if (video.getVideoUri().toString().toLowerCase().endsWith(".asx")) {
                 String newUri = AsxParser.getUri(video.getVideoUri().toString());
                 video.setVideoUri(new URI(newUri));
             }
             return page;
         } else {
-            if(page.getUri().toString().equals(OVERVIEW_ABZ)) {
-                IOverviewPage opage = (IOverviewPage)page;
+            if (page.getUri().toString().equals(OVERVIEW_ABZ)) {
+                IOverviewPage opage = (IOverviewPage) page;
                 opage.getPages().clear();
-                for (Entry<String,String> abzPage : aBisZ.entrySet()) {
+                for (Entry<String, String> abzPage : aBisZ.entrySet()) {
                     String title = abzPage.getKey();
                     String uri = abzPage.getValue();
                     OverviewPage tmp = new OverviewPage();
@@ -119,36 +119,36 @@ public class ZDFMediathekParser implements IWebParser {
             return page;
         }
     }
-    
+
     private void parseVideoPage(IVideoPage video) throws IOException, ParserException, URISyntaxException, ParseException {
         String content = HttpUtils.get(video.getUri().toString(), null, CHARSET);
-        
+
         // parse the title
         video.setTitle(Translate.decode(HtmlParserUtils.getText(content, CHARSET, "h1.beitragHeadline")));
-        
+
         // parse the description
         video.setDescription(Translate.decode(HtmlParserUtils.getText(content, CHARSET, "div.beitrag p.kurztext")));
-        
+
         // parse the thumbnail image
         ImageTag img = (ImageTag) HtmlParserUtils.getTag(content, CHARSET, "div.beitrag img");
-        video.setThumbnail(new URI (BASE_URI + img.getImageURL()));
-        
+        video.setThumbnail(new URI(BASE_URI + img.getImageURL()));
+
         // parse the video uri
         NodeList videoLinks = HtmlParserUtils.getTags(content, CHARSET, "ul.dslChoice li a");
         LinkTag dsl2000 = (LinkTag) videoLinks.elementAt(1);
         video.setVideoUri(new URI(dsl2000.extractLink()));
-        
+
         // parse the pubDate
         String datum = HtmlParserUtils.getText(content, CHARSET, "p.datum");
-        datum = datum.substring(datum.lastIndexOf(',')+1).trim();
+        datum = datum.substring(datum.lastIndexOf(',') + 1).trim();
         Date pubDate = new SimpleDateFormat("dd.MM.yyyy").parse(datum);
         Calendar cal = Calendar.getInstance();
         cal.setTime(pubDate);
         video.setPublishDate(cal);
-        
+
         // parse the duration
         Matcher m = Pattern.compile("VIDEO,\\s+(\\d+):(\\d+)").matcher(content);
-        if(m.find()) {
+        if (m.find()) {
             int minutes = Integer.parseInt(m.group(1));
             int seconds = Integer.parseInt(m.group(2));
             video.setDuration(60 * minutes + seconds);
@@ -159,45 +159,45 @@ public class ZDFMediathekParser implements IWebParser {
         // clear the parsed pages, so that we don't have duplicate results, if a user
         // opens this page multiple times
         page.getPages().clear();
-        
+
         // set the number of displayed items on the page to 1000
         String uri = HttpUtils.addParameter(page.getUri().toString(), "teaserListIndex", "1000");
-        
+
         String content = HttpUtils.get(uri, null, CHARSET);
         NodeList titles = HtmlParserUtils.getTags(content, CHARSET, "div.text");
         NodeIterator iter = titles.elements();
-        while(iter.hasMoreNodes()) {
+        while (iter.hasMoreNodes()) {
             Div div = (Div) iter.nextNode();
-            NodeList links = (NodeList) HtmlParserUtils.getTags(div.toHtml(), CHARSET, "a");
+            NodeList links = HtmlParserUtils.getTags(div.toHtml(), CHARSET, "a");
             LinkTag a = (LinkTag) links.elementAt(1);
-            
+
             // parse page uri
             String pageUri = BASE_URI + Translate.decode(a.extractLink());
-            
+
             // parse page title
             String title = Translate.decode(a.getLinkText()).trim();
-            
+
             // detect page type
             LinkTag typeLink = (LinkTag) links.elementAt(2);
             boolean isVideo = Translate.decode(typeLink.getParent().toPlainTextString()).trim().toLowerCase().contains("video");
-            
+
             // create an OverviewPage or a VideoPage
             IWebPage subPage = null;
             URI subPageUri = new URI(pageUri.replaceAll(" ", "+"));
-            if(isOverviewPage(subPageUri)) {
+            if (isOverviewPage(subPageUri)) {
                 subPage = new OverviewPage();
             } else {
                 VideoPage tmp = new VideoPage();
                 // parse the pubDate
-                String datum = ((LinkTag)links.elementAt(0)).getLinkText();
+                String datum = ((LinkTag) links.elementAt(0)).getLinkText();
                 try {
-                    datum = datum.substring(datum.lastIndexOf(',')+1).trim();
+                    datum = datum.substring(datum.lastIndexOf(',') + 1).trim();
                     Date pubDate = new SimpleDateFormat("dd.MM.yyyy").parse(datum);
                     Calendar cal = Calendar.getInstance();
                     cal.setTime(pubDate);
                     tmp.setPublishDate(cal);
                 } catch (Throwable t) {
-                    logger.log(LogService.LOG_WARNING, "Couldn't parse date "+datum+" for page " + title);
+                    logger.log(LogService.LOG_WARNING, "Couldn't parse date " + datum + " for page " + title);
                     tmp.setPublishDate(Calendar.getInstance());
                 }
                 subPage = tmp;
@@ -205,11 +205,11 @@ public class ZDFMediathekParser implements IWebParser {
             subPage.setParser(ID);
             subPage.setTitle(title);
             subPage.setUri(subPageUri);
-            
+
             // if the subpage is an item page and if it is video, add it to the overview page
             // otherwise it is an overview page and we add it, too
             if (!isOverviewPage(subPageUri)) {
-                if(isVideo) {
+                if (isVideo) {
                     page.getPages().add(subPage);
                 }
             } else {
@@ -217,11 +217,11 @@ public class ZDFMediathekParser implements IWebParser {
             }
         }
     }
-    
+
     private boolean isOverviewPage(URI uri) {
         return uri != null && uri.getPath() != null && uri.getPath().matches(".*/\\d+");
     }
-    
+
     @Override
     public String getId() {
         return ID;
