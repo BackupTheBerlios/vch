5fce85483b80b6e7a81fd932a876b7d18047e3ec
Henrik Niehaus <henrik.niehaus@gmx.de>
2012-03-10 18:08:45 +0100

    added http format to VideoTypeComparator

--- vch-parser-ard/src/main/java/de/berlios/vch/parser/ard/VideoTypeComparator.java
+++ vch-parser-ard/src/main/java/de/berlios/vch/parser/ard/VideoTypeComparator.java
@@ -6,34 +6,34 @@ import java.util.Map;
 
 import de.berlios.vch.parser.ard.VideoItemPageParser.VideoType;
 
-
 /**
  * Compares two videos according to their type and quality
- *
+ * 
  * @author <a href="mailto:hampelratte@users.berlios.de">hampelratte@users.berlios.de</a>
  */
 public class VideoTypeComparator implements Comparator<VideoType> {
 
     private Map<Integer, Integer> typePriorities = new HashMap<Integer, Integer>();
-    
+
     public VideoTypeComparator() {
         // flash has higher priority than wmv
-        typePriorities.put(0, 1); // flash
+        typePriorities.put(0, 2); // flash
+        typePriorities.put(1, 1); // http
         typePriorities.put(2, 0); // wmv
     }
-    
+
     @Override
     public int compare(VideoType vt1, VideoType vt2) {
-        if(typePriorities.get(vt1.getFormat()) > typePriorities.get(vt2.getFormat())) {
+        if (typePriorities.get(vt1.getFormat()) > typePriorities.get(vt2.getFormat())) {
             return -1;
-        } else if(typePriorities.get(vt1.getFormat()) < typePriorities.get(vt2.getFormat())) {
+        } else if (typePriorities.get(vt1.getFormat()) < typePriorities.get(vt2.getFormat())) {
             return 1;
-        } else if(vt1.getQuality() > vt2.getQuality()) {
+        } else if (vt1.getQuality() > vt2.getQuality()) {
             return -1;
-        } else if(vt1.getQuality() < vt2.getQuality()) {
+        } else if (vt1.getQuality() < vt2.getQuality()) {
             return 1;
         }
-        
+
         return 0;
     }
 }
