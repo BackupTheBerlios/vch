a62a5ae74095a838bf8087123d11d7fce2843f63
Henrik Niehaus <henrik.niehaus@gmx.de>
2012-03-10 17:03:57 +0100

    made the 3sat feed parser more robust

--- vch-parser-3sat/src/main/java/de/berlios/vch/parser/dreisat/DreisatParser.java
+++ vch-parser-3sat/src/main/java/de/berlios/vch/parser/dreisat/DreisatParser.java
@@ -41,23 +41,23 @@ import de.berlios.vch.parser.WebPageTitleComparator;
 public class DreisatParser implements IWebParser {
 
     public static final String BASE_URL = "http://www.3sat.de";
-    
+
     public static final String MEDIATHEK_URL = BASE_URL + "/mediathek/";
-    
+
     private static final String LANDING_PAGE = MEDIATHEK_URL + "mediathek.php?mode=rss";
-    
+
     public static final String CHARSET = "iso-8859-15";
-    
+
     public static final String ID = DreisatParser.class.getName();
-    
-    public static Map<String,String> HTTP_HEADERS = new HashMap<String, String>();
+
+    public static Map<String, String> HTTP_HEADERS = new HashMap<String, String>();
     static {
         HTTP_HEADERS.put("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090821 Gentoo Firefox/3.5.2");
         HTTP_HEADERS.put("Accept-Language", "de-de,de;q=0.8,en-us;q=0.5,en;q=0.3");
     }
-    
+
     public DreisatFeedParser feedParser;
-    
+
     @Requires
     private LogService logger;
 
@@ -67,14 +67,14 @@ public class DreisatParser implements IWebParser {
         page.setParser(ID);
         page.setTitle(getTitle());
         page.setUri(new URI("vchpage://localhost/" + getId()));
-        
-        // add all rss feeds to 
+
+        // add all rss feeds to
         String landingPage = HttpUtils.get(LANDING_PAGE, HTTP_HEADERS, CHARSET);
         NodeList links = HtmlParserUtils.getTags(landingPage, CHARSET, "div.rss a.link");
         NodeIterator iter = links.elements();
-        while(iter.hasMoreNodes()) {
+        while (iter.hasMoreNodes()) {
             Node child = iter.nextNode();
-            if(child instanceof LinkTag) {
+            if (child instanceof LinkTag) {
                 LinkTag link = (LinkTag) child;
                 String pageUri = MEDIATHEK_URL + link.extractLink();
                 String title = Translate.decode(link.getLinkText()).trim().substring(1);
@@ -85,7 +85,7 @@ public class DreisatParser implements IWebParser {
                 page.getPages().add(feedPage);
             }
         }
-        
+
         // add the general 3sat mediathek feed
         OverviewPage feedPage = new OverviewPage();
         feedPage.setParser(ID);
@@ -103,20 +103,20 @@ public class DreisatParser implements IWebParser {
 
     @Override
     public IWebPage parse(IWebPage page) throws Exception {
-        if(page instanceof VideoPage) {
+        if (page instanceof VideoPage) {
             VideoPage vpage = (VideoPage) page;
             if (vpage.getVideoUri().toString().endsWith(".asx")) {
                 String uri = AsxParser.getUri(vpage.getVideoUri().toString());
                 vpage.setVideoUri(new URI(uri));
                 page.getUserData().remove("video");
             }
-            
+
             String content = HttpUtils.get(vpage.getUri().toString(), null, CHARSET);
             ImageTag img = (ImageTag) HtmlParserUtils.getTag(content, CHARSET, "div.seite div.media img.still");
-            if(img != null) {
+            if (img != null) {
                 vpage.setThumbnail(new URI(BASE_URL + img.getImageURL()));
             }
-            
+
             return page;
         } else {
             SyndFeed feed = feedParser.parse(page);
@@ -124,32 +124,38 @@ public class DreisatParser implements IWebParser {
             feedPage.setParser(ID);
             feedPage.setTitle(page.getTitle());
             feedPage.setUri(page.getUri());
-            
+
             for (Iterator<?> iterator = feed.getEntries().iterator(); iterator.hasNext();) {
                 SyndEntry entry = (SyndEntry) iterator.next();
+
+                if (entry.getEnclosures().size() == 0) {
+                    continue;
+                }
+
                 VideoPage video = new VideoPage();
                 video.setParser(ID);
                 video.setTitle(entry.getTitle());
-                if(entry.getDescription() != null) {
-                	video.setDescription(entry.getDescription().getValue());
+                if (entry.getDescription() != null) {
+                    video.setDescription(entry.getDescription().getValue());
                 }
                 video.setUri(new URI(entry.getLink()));
                 Calendar pubCal = Calendar.getInstance();
                 pubCal.setTime(entry.getPublishedDate());
                 video.setPublishDate(pubCal);
-                video.setVideoUri( new URI( ((SyndEnclosure)entry.getEnclosures().get(0)).getUrl() ) );
-                
+                video.setVideoUri(new URI(((SyndEnclosure) entry.getEnclosures().get(0)).getUrl()));
+
                 // look, if we have a duration in the foreign markup
                 @SuppressWarnings("unchecked")
-                List<Element> fm = (List<Element>)entry.getForeignMarkup();
+                List<Element> fm = (List<Element>) entry.getForeignMarkup();
                 for (Element element : fm) {
-                    if("duration".equals(element.getName())) {
+                    if ("duration".equals(element.getName())) {
                         try {
                             video.setDuration(Long.parseLong(element.getText()));
-                        } catch(Exception e) {}
+                        } catch (Exception e) {
+                        }
                     }
                 }
-                
+
                 feedPage.getPages().add(video);
             }
             return feedPage;
@@ -162,8 +168,9 @@ public class DreisatParser implements IWebParser {
     }
 
     @Invalidate
-    public void stop() {}
-    
+    public void stop() {
+    }
+
     @Override
     public String getId() {
         return ID;
